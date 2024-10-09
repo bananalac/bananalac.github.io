46ba2707735131c5752a6a9a54c7ee630ba5345c
Kaveh <81914120+kavascriptos@users.noreply.github.com>
2024-10-09 10:43:19 +0330

    Removed linter

--- assets/js/linter/commands/minor.js
+++
@@ -1,8 +0,0 @@
-import commandFree from "./command_free.js";
-import commandHasOption from "./command_has_option.js";
-import commandMultiOption from "./command_multi_options.js";
-import conditional from "./ifcondtion.js";
-import noOption from "./no_option.js";
-import subcommand from "./subcommand.js";
-
-export { subcommand, commandHasOption, noOption, commandFree,commandMultiOption, conditional };
--- assets/js/linter/commands/no_option.js
+++
@@ -1,5 +0,0 @@
-export default function noOption(func, value, args) {
-    const [reportBody, errors, warnings, suggestions, lineNum] = [args[0], args[1], args[2], args[3], args[4]];
-
-    if(value.trim() !== '') errors.push(`${func}{} should not have any options. (Line ${lineNum})`)
-}
